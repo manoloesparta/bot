a5aeedce8cf20177c2e0b2991cf78df277aa6588
Manolo <manolototopoyo@hotmail.com>
2017-04-06 23:05:07 -0700

    Trump Finish

--- string_cleaner.py
+++ string_cleaner.py
@@ -1,3 +1,22 @@
-archivo = open('string.txt','r')
-print(archivo)
-archivo.close()
+def string_cleaner(string):
+    nuevo = ''
+    while True:
+        inicio = string.find('>', len(nuevo))
+        final = string.find('<', inicio)
+        nuevo += string[inicio:final+1]
+        if inicio == -1:
+            break
+
+    nuevo = nuevo.split('<>')
+    nuevo = list(filter(None, nuevo))
+
+    visto = set()
+    nuevo_2 = []
+    for i in nuevo:
+        if i not in visto:
+            visto.add(i)
+            nuevo_2.append(i)
+
+    nuevo = ''.join(nuevo_2)
+
+    return nuevo[1:-1]
--- trump.py
+++ trump.py
@@ -1,9 +1,13 @@
 def trump_tweet():
+    from string_cleaner import string_cleaner
     from urllib.request import urlopen
     html = urlopen("https://twitter.com/realDonaldTrump").read().decode('utf-8')
-    a = html.find('TweetTextSize TweetTextSize--large js-tweet-text tweet-text" lang="en" data-aria-label-part="0">')
-    b = html.find('</p>', a + 96)
-    
-    return '@realDonaldTrump says : ' + html[a+96:b-1]
+    a = html.find('<div class="content">')
+    b = html.find('</p>', a)
 
+    if html[a+2424:b].find('@') == -1 and html[a+2424:b].find('#') == -1:
+        return '@realDonaldTrump says : ' + html[a + 2424:b]
+    else:
+        html = '>' + html[a + 2424:b] + '<'
+        return string_cleaner(html)[:-1]
 print(trump_tweet())
